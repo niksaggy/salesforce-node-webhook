7e6fa8303c3a861d62d3da0cfcde3cdd4f4a6790
Sagnik <60902566+niksaggy@users.noreply.github.com>
2020-03-15 17:01:00 +0100

    Update app.js

--- app.js
+++ app.js
@@ -7,42 +7,41 @@ var options;
 var port = process.env.PORT || 3000;
 
 const expApp = express().use(bodyParser.json());
-
-const oauth2 = new jsforce.OAuth2({
-    clientId: process.env.SALESFORCE_CONSUMER_KEY,
-    clientSecret: process.env.SALESFORCE_CONSUMER_SECRET,
-    redirectUri: 'https://sfdcadminbot.herokuapp.com/getaccesstoken'
-});
-
 expApp.use(bodyParser.urlencoded());
 
 
 expApp.get('/oauth2/auth', function(req, res) {
 	console.log(req);
 	res.redirect(oauth2.getAuthorizationUrl({}));
+	res.send('success');
 });
+
 
 expApp.get('/getAccessToken', function(req,resp) {
 	console.log('should be here ');
-	
+	const oauth2 = new jsforce.OAuth2({
+		clientId: process.env.SALESFORCE_CONSUMER_KEY,
+		clientSecret: process.env.SALESFORCE_CONSUMER_SECRET,
+		redirectUri: 'https://sfdcadminbot.herokuapp.com/getAccessToken'
+	});
 
-		const conn = new jsforce.Connection({ oauth2 : oauth2 });
-		console.log('req query code: '+req);
-		console.log('req query code: '+req.query);
-		//or do we do the redirection here?
+	const conn = new jsforce.Connection({ oauth2 : oauth2 });
+		console.log('req query code '+req.query.code);
 		conn.authorize(req.query.code, function(err, userInfo) {
-			if (err) {
-				console.log('Error happened at authorization-->',err);
-				return resp.send(err.message);
-			}
-			console.log('access token',conn.accessToken);
-			console.log('refresh token',conn.refreshToken);
-			console.log(conn.instanceUrl);
-			console.log("User ID: " + userInfo.id);
-			console.log("Org ID: " + userInfo.organizationId);
-			options = { Authorization: 'Bearer '+conn.accessToken};
+		if (err) {
+            console.log('Error happened at authorization-->',err);
+			return resp.send(err.message);
+		}
+		console.log("user ID: " + res.user_id);
+		console.log("organization ID: " + res.organization_id);
+		console.log("username: " + res.username);
+		console.log("display name: " + res.display_name);
+		options = { Authorization: 'Bearer '+conn.accessToken};
+		console.log('authorization bearer: '+options);
+		//resp.redirect(`https://oauth-redirect.googleusercontent.com/r/YOUR_PROJECT_ID?code=${req.query.code}&state=true`);
 		});
-	});	
+	});
+	
 });
 
 
@@ -54,5 +53,5 @@ expApp.get('/', function (req, res) {
 });
 expApp.listen(port, function () {
 	
-	console.log('Example app listening on port 3000!');
+	console.log('Example app listening on port ! ${port}');
 });
